2263a65ee9088cd201440585d83498dbe81247a2
Subin Jeong <subin.jeong@illuminarean.com>
2021-08-28 15:34:39 +0900

    function extract

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ var invoice = require("./invoices.json");
 var plays = require("./plays.json");
 
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `청구 내역(고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
 
