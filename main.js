50b58589b42d0d661961beac364aa12f8b4a8755
Subin Jeong <subin.jeong@illuminarean.com>
2021-08-30 21:57:19 +0900

    seperate files

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 var invoice = require("./invoices.json");
 var plays = require("./plays.json");
 
-function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
-}
+var createStatementData = require("./createStatementData.js").createStatementData;
 
-function enrichPerformance(aPerformance) {
-  const result = Object.assign({}, aPerformance);
-  result.play = playFor(result);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
-  return result;
+function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
 }
 
 function renderPlainText(data, plays) {
@@ -31,55 +20,12 @@ function renderPlainText(data, plays) {
   return result;
 }
 
-function totalAmount(data) {
-  return data.performances.reduce((total, p) => total + p.amount, 0);
-}
-
-function totalVolumeCredits(data) {
-  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-}
-
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(aNumber / 100);
-}
-
-function volumeCreditsFor(aPerformance) {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === aPerformance.play.type) {
-    volumeCredits += Math.floor(aPerformance.audience / 5);
-  }
-  return volumeCredits;
-}
-
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-  }
-  return result;
 }
 
 console.log(statement(invoice, plays));
